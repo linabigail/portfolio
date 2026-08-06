7e19545795419b73523c1f6d61c9c0fb0d87262f
Abigail Lin <linabigail327@gmail.com>
2026-08-05 23:27:13 -0400

    added footnote

---
+++ src/content/side.config.ts
@@ -0,0 +1,15 @@
+export const siteConfig = {
+    name: "Abigail Lin",
+    tagline: "Electrical Engineer at Purdue University",
+    nav: [
+        { label: "Home", href: "/"},
+        { label: "Projects", href: "/projects" },
+        { label: "Experience", href: "/experience" },
+        { label: "Contact", href: "/contact" },
+    ],
+    socials: [
+        { label: "Github", href: "https://github.com/linabigail"},
+        { label: "LinkedIn", href: "https://www.linkedin.com/in/abigail-l-888929269/"},
+        { label: "Email", href: "linabigail327@gmail.com" },
+    ],
+};
